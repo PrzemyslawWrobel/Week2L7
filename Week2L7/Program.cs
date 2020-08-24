80400b61730982308aacc55aba1791af70276482
PW <przemek.wrobel@poczta.fm>
2020-08-24 12:08:47 +0200

    Zadanie_3

--- Week2L7/Program.cs
+++ Week2L7/Program.cs
@@ -8,10 +8,38 @@ namespace Week2L7
         {
             Zadanie_1();
             Zadanie_2();
+            Zadanie_3();
+          
+
+        }
+
+        private static void Zadanie_3()
+        {
+            Console.WriteLine("Sprawdzamy czy liczba jest dodatnia czy ujemna");
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+            //string number = Console.ReadLine();
+            // int chosenNumber; 
+            if ((Int32.TryParse(Console.ReadLine(), out int chosenNumber) == true) && chosenNumber !=0)
+            {
+                if (chosenNumber > 0)
+                {
+                    Console.WriteLine($"Podana liczba: {chosenNumber} jest dodatnia");
+                }
+                else
+                {
+                    Console.WriteLine($"Podana liczba: {chosenNumber} jest ujemna");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Podana liczba: {chosenNumber} nie jest ujemna ani dodatania");
+            }
+
         }
 
         private static void Zadanie_2()
         {
+            Console.WriteLine("Sprawdzamy czy liczba jest parzysta");
             Console.WriteLine("Podaj liczbę całkowitą: ");
             //string number = Console.ReadLine();
             // int chosenNumber; 
