05e756267146d6801467cef5f0713931edcef38d
PW <przemek.wrobel@poczta.fm>
2020-08-24 11:49:42 +0200

    Zadanie_1 i Zadanie_2

---
+++ Week2L7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Week2L7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zadanie_1();
+            Zadanie_2();
+        }
+
+        private static void Zadanie_2()
+        {
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+            //string number = Console.ReadLine();
+            // int chosenNumber; 
+            if (Int32.TryParse(Console.ReadLine(), out int chosenNumber) == true)
+            {
+                if (chosenNumber % 2 == 0)
+                {
+                    Console.WriteLine($"Podana liczba: {chosenNumber} jest liczbą parzystą");
+                }
+                else
+                {
+                    Console.WriteLine($"Podana liczba: {chosenNumber} jest liczbą nieparzystą");
+                }
+            }
+            
+
+        }
+
+        private static void Zadanie_1()
+        {
+            int a = 5;
+            int b = 5;
+
+            if(a==b)
+            {
+               Console.WriteLine($"Liczby {a} i {b} są równe");
+            }
+            else
+            {
+                Console.WriteLine($"Liczby {a} i {b} nie są równe");
+            }
+        }
+    }
+}
